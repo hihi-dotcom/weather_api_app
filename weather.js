35da04e508ba1c8a38c090bc01a927e7cefd253b
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-10-02 20:05:06 +0200

    kiírja az adatokat

--- weather.js
+++ weather.js
@@ -16,15 +16,32 @@ function IdojarasKereses(){
         resp.json()
     )
     .then(adat => 
-        console.log(adat)
+        puttingDataIntoSpans(adat)
+
     )
     .catch(() => 
         console.log("A beírt város nem található!!!")
     )
 }
+function puttingDataIntoSpans(tomb){
+    $("nev").innerHTML = tomb.name;
+    $("idojaras").innerHTML = tomb.weather[0].main;
+    $("idojaras_bo").innerHTML = tomb.weather[0].description;
+    fetchingIcon(tomb.weather[0].icon);
+    $("hom").innerHTML = tomb.main.temp;
+    $("hom_erzes").innerHTML = tomb.main.feels_like;
+    $("legnyomas").innerHTML = tomb.main.pressure;
+    $("para").innerHTML = tomb.main.humidity;
+    $("szel").innerHTML = tomb.wind.speed;
+    $("orszag").innerHTML = tomb.sys.country;
 
+}
 
-
+function fetchingIcon(bemeneti_tomb_prop){
+    const url = `https://openweathermap.org/img/wn/${bemeneti_tomb_prop}@2x.png`;
+     $("idojaras_ikon").innerHTML = `<img src="${url}">`;
+;
+}
 $('button-kereses').addEventListener('click', IdojarasKereses);
 $('varos-kereses').addEventListener('keypress', e => {
     if(e.key === 'Enter'){
