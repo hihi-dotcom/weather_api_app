411cdd20f2d23889a9b3a0901e8a2923800748ab
Czirjak Adam <czirjak.adam@bolyai.local>
2025-10-03 10:01:33 +0200

    bootstrap is van a weatherappabn

--- weather.js
+++ weather.js
@@ -19,28 +19,33 @@ function IdojarasKereses(){
         puttingDataIntoSpans(adat)
 
     )
-    .catch(() => 
-        console.log("A beírt város nem található!!!")
+    .catch((err) => 
+        console.log(err)
     )
 }
 function puttingDataIntoSpans(tomb){
-    $("nev").innerHTML = tomb.name;
+
     $("idojaras").innerHTML = tomb.weather[0].main;
     $("idojaras_bo").innerHTML = tomb.weather[0].description;
     fetchingIcon(tomb.weather[0].icon);
-    $("hom").innerHTML = tomb.main.temp;
-    $("hom_erzes").innerHTML = tomb.main.feels_like;
-    $("legnyomas").innerHTML = tomb.main.pressure;
-    $("para").innerHTML = tomb.main.humidity;
-    $("szel").innerHTML = tomb.wind.speed;
+    $("hom").innerHTML = Math.round(FromKelvinToCel(tomb.main.temp)) + "°C";
+    $("hom_erzes").innerHTML = Math.round(FromKelvinToCel(tomb.main.feels_like))  + "°C";
+    $('min_hom').innerHTML = Math.round(FromKelvinToCel(tomb.main.temp_min)) + "°C";
+    $('max_hom').innerHTML = Math.round(FromKelvinToCel(tomb.main.temp_max)) + "°C";
+    $("legnyomas").innerHTML = tomb.main.pressure + " Pa";
+    $("para").innerHTML = tomb.main.humidity + " %";
+    $("szel").innerHTML = tomb.wind.speed + " km/h";
     $("orszag").innerHTML = tomb.sys.country;
 
 }
 
 function fetchingIcon(bemeneti_tomb_prop){
     const url = `https://openweathermap.org/img/wn/${bemeneti_tomb_prop}@2x.png`;
-     $("idojaras_ikon").innerHTML = `<img src="${url}">`;
+     $("idojaras_ikon").src = url;
 ;
+}
+function FromKelvinToCel(bemeneti_ertek){
+    return bemeneti_ertek - 273.15;
 }
 $('button-kereses').addEventListener('click', IdojarasKereses);
 $('varos-kereses').addEventListener('keypress', e => {
